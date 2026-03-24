a2e56f37bd87c080ab8f3c425000ced3073002d9
kimnzh <hakimnizami15@gmail.com>
2026-03-25 00:48:55 +0700

    fix: any type

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default async function RootLayout({
           where: { id: session.user.id },
           select: { canEditTheme: true },
         })
-        .then((u) => u?.canEditTheme ?? false)
+        .then((u: { canEditTheme: boolean } | null) => u?.canEditTheme ?? false)
     : false;
 
   return (
